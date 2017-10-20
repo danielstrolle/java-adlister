298a448e17461b43a54852b275a4b9785abe42ec
danielstrolle <danielstrolle@gmail.com>
2017-10-20 11:40:43 -0500

    create config.java and added to .gitignore
    * create private and final properties in our config to be able to make our connection secure. create getters for our properties

---
+++ Config.java
@@ -0,0 +1,17 @@
+public class Config {
+    private final String url = "";
+    private final String user = "";
+    private final String password = "";
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
