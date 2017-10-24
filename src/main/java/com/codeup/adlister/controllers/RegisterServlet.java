404a3f87666d1611d1a7a498068cb6fadf4d3fb9
danielstrolle <danielstrolle@gmail.com>
2017-10-24 14:41:28 -0500

    Complete the passwords exercise
    * Make sure passwords are being hashed before they enter the database.
    
    * Change you login logic to check against users' hashed passwords.
    
    * Don't allow ad creation unless a user is logged in.
    
    * When a new ad is created, assign the user id of the logged in user to the ad.

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,6 +21,7 @@ public class RegisterServlet extends HttpServlet {
         String username = request.getParameter("username");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
+
         String passwordConfirmation = request.getParameter("confirm_password");
 
         // validate input
@@ -32,9 +34,11 @@ public class RegisterServlet extends HttpServlet {
             response.sendRedirect("/register");
             return;
         }
+        int numberOfRounds = 12;
+        String hash = BCrypt.hashpw(password, BCrypt.gensalt(numberOfRounds));
 
         // create and save a new user
-        User user = new User(username, email, password);
+        User user = new User(username, email, hash);
         DaoFactory.getUsersDao().insert(user);
         response.sendRedirect("/login");
     }
